448f3ffbaddfb14756c0336c4ab408180467605d
DatadudeDev <132722687+DatadudeDev@users.noreply.github.com>
2023-07-23 23:23:07 -0700

    Update maps_custom_model.dart

--- lib/components/maps_custom/maps_custom_model.dart
+++ lib/components/maps_custom/maps_custom_model.dart
@@ -1,3 +1,4 @@
+import '/auth/firebase_auth/auth_util.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
@@ -5,21 +6,153 @@ import '/flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'map_google_model.dart';
+export 'map_google_model.dart';
 
-class MapsCustomModel extends FlutterFlowModel {
-  ///  State fields for stateful widgets in this component.
+class MapGoogleWidget extends StatefulWidget {
+  const MapGoogleWidget({
+    Key? key,
+    this.parameter1,
+  }) : super(key: key);
 
-  // State field(s) for GoogleMap widget.
-  LatLng? googleMapsCenter;
-  final googleMapsController = Completer<GoogleMapController>();
+  final LatLng? parameter1;
 
-  /// Initialization and disposal methods.
+  @override
+  _MapGoogleWidgetState createState() => _MapGoogleWidgetState();
+}
 
-  void initState(BuildContext context) {}
+class _MapGoogleWidgetState extends State<MapGoogleWidget> {
+  late MapGoogleModel _model;
 
-  void dispose() {}
+  LatLng? currentUserLocationValue;
 
-  /// Action blocks are added here.
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
 
-  /// Additional helper methods are added here.
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => MapGoogleModel());
+
+    getCurrentUserLocation(defaultLocation: LatLng(0.0, 0.0), cached: true)
+        .then((loc) => setState(() => currentUserLocationValue = loc));
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentUserLocationValue == null) {
+      return Container(
+        color: FlutterFlowTheme.of(context).primaryBackground,
+        child: Center(
+          child: SizedBox(
+            width: 50.0,
+            height: 50.0,
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(
+                FlutterFlowTheme.of(context).primary,
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return StreamBuilder<List<UserPostsRecord>>(
+      stream: queryUserPostsRecord(
+        queryBuilder: (userPostsRecord) =>
+            userPostsRecord.where('postUser', isEqualTo: currentUserReference),
+      ),
+      builder: (context, snapshot) {
+        // Customize what your widget looks like when it's loading.
+        if (!snapshot.hasData) {
+          return Center(
+            child: SizedBox(
+              width: 50.0,
+              height: 50.0,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  FlutterFlowTheme.of(context).primary,
+                ),
+              ),
+            ),
+          );
+        }
+        List<UserPostsRecord> containerUserPostsRecordList = snapshot.data!;
+        return Container(
+          width: 165.0,
+          height: 120.0,
+          decoration: BoxDecoration(
+            color: Color(0xAB000000),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: StreamBuilder<List<LocationVotesRecord>>(
+            stream: queryLocationVotesRecord(),
+            builder: (context, snapshot) {
+              // Customize what your widget looks like when it's loading.
+              if (!snapshot.hasData) {
+                return Center(
+                  child: SizedBox(
+                    width: 50.0,
+                    height: 50.0,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        FlutterFlowTheme.of(context).primary,
+                      ),
+                    ),
+                  ),
+                );
+              }
+              List<LocationVotesRecord> containerLocationVotesRecordList =
+                  snapshot.data!;
+              return Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  color: FlutterFlowTheme.of(context).secondaryBackground,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: FlutterFlowGoogleMap(
+                  controller: _model.googleMapsController,
+                  onCameraIdle: (latLng) =>
+                      setState(() => _model.googleMapsCenter = latLng),
+                  initialLocation: _model.googleMapsCenter ??=
+                      currentUserLocationValue!,
+                  markers: containerLocationVotesRecordList
+                      .map(
+                        (containerLocationVotesRecord) => FlutterFlowMarker(
+                          containerLocationVotesRecord.reference.path,
+                          containerLocationVotesRecord.voteLocation!,
+                        ),
+                      )
+                      .toList(),
+                  markerColor: GoogleMarkerColor.orange,
+                  mapType: MapType.hybrid,
+                  style: GoogleMapStyle.standard,
+                  initialZoom: 3.0,
+                  allowInteraction: true,
+                  allowZoom: true,
+                  showZoomControls: false,
+                  showLocation: false,
+                  showCompass: false,
+                  showMapToolbar: false,
+                  showTraffic: false,
+                  centerMapOnMarkerTap: true,
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
 }
